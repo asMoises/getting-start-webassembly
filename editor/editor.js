1d1e140951d72b782bbce07be960d3ba0eb5ecc5
Moisés Silva <moiss.silva@outlook.com>
2025-03-16 17:40:15 -0300

    Testes de acesso e criação de memória com JS e JS acessando WASM

--- editor/editor.js
+++ editor/editor.js
@@ -1,10 +1,40 @@
-const title_test = document.getElementById("test_title");
+// **************** JS Area
+let memoria = null;
+const comprimento = 10;
+const ponteiro = 5;
 
-// Se não tiver compilado o arquivo: use 'editor.wasm'
+function criar_memoria_inicial(comprimento, ponteiro) {
+  memoria = new Array(comprimento).fill(0);
+  memoria[ponteiro] = 85;
+}
+
+criar_memoria_inicial(comprimento, ponteiro);
+console.log("Memoria using just JS: \n" + memoria);
+
+// ********* Acess WebAssembly using JS
 const arquivo = "./target/wasm32-unknown-unknown/release/editor.wasm";
-// const arquivo = 'editor.wasm';
 
-WebAssembly.instantiateStreaming(fetch(arquivo)).then(({ instance }) => {
-  const { subtracao } = instance.exports;
-  title_test.textContent = "Subtração: " + subtracao(28, 10);
+WebAssembly.instantiateStreaming(fetch(arquivo)).then((wasm) => {
+  const { instance } = wasm;
+  const { subtracao, criar_memoria_inicial, memory, malloc, acumular } =
+    instance.exports;
+
+  criar_memoria_inicial();
+  const arraMemoria = new Uint8Array(memory.buffer, 0).slice(0, 10);
+  console.log("Memory Aceess by WASM: \n" + arraMemoria);
+  console.log(subtracao(28, 10));
+
+  const jsLista = Uint8Array.from([20, 50, 80]);
+  const comprimento = jsLista.length;
+  const wasmListaPonteiro = malloc(comprimento);
+
+  const wasmLista = new Uint8Array(
+    memory.buffer,
+    wasmListaPonteiro,
+    comprimento
+  );
+
+  wasmLista.set(jsLista);
+  const somaEntreItensDaLista = acumular(wasmListaPonteiro, comprimento);
+  console.log(somaEntreItensDaLista);
 });
--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -1,5 +1,48 @@
-// uso do no_mangle para inserir a função no binario
+use core::slice::from_raw_parts_mut;
+use std::alloc::{alloc,Layout};
+use std::mem;
 
+
+#[no_mangle]
+extern fn acumular(ponteiro: *mut u8, comprimento: usize) -> i32 {
+    let fatia = unsafe { from_raw_parts_mut(ponteiro as *mut u8, comprimento) };
+    let mut soma = 0;
+    for i in 0..comprimento {
+        soma = soma + fatia[i];
+    }
+    soma as i32
+}
+
+#[no_mangle]
+extern fn malloc(comprimento: usize) -> *mut u8 {
+    let alinhamento = mem::align_of::<usize>();
+    if let Ok(layout) = Layout::from_size_align(comprimento, alinhamento) {
+        unsafe {
+            if layout.size() > 0 {
+                let ponteiro = alloc(layout);
+                if !ponteiro.is_null() {
+                    return ponteiro;
+                }
+            } else {
+                return alinhamento as *mut u8;
+            }
+        }
+    }
+    std::process::abort()
+}
+
+#[no_mangle]
+extern fn criar_memoria_inicial(){
+    let fatia: &mut [u8];
+    unsafe {
+        // começa em 5 e vai ate 15, pois tem 10 posições
+        fatia = from_raw_parts_mut::<u8>(5 as *mut u8, 10);
+    }
+    // na posição zero, qu eé 5, está o valor 85
+    fatia[0]= 85
+}
+
+// uso do no_mangle para inserir a função no binario
 #[no_mangle]
 pub extern fn subtracao(numero_a: u8, numero_b: u8) -> u8{
     numero_a - numero_b
